d39ff9f087dc52447f3cba19682b5ec366a16b3f
OzodbekXolmurodov <ozodbekxolmurodov08@gmail.com>
2024-11-12 23:53:23 +0500

    praect

--- src/pages/shopping/Shopping.jsx
+++ src/pages/shopping/Shopping.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import Dastavka from "../../components/dastavka/DAstavka";
+// import Dastavka from "../../components/dastavka/DAstavka";
 
 const Shopping = () => {
-  return (
-    <div>
-      <Dastavka />
-    </div>
-  );
+  return <div>{/* <Dastavka /> */}</div>;
 };
 
 export default Shopping;
