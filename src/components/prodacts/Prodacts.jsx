f66544c6bb2a8ba5fcf1060874f5db81e3a2cc2d
OzodbekXolmurodov <ozodbekxolmurodov08@gmail.com>
2024-11-14 19:52:20 +0500

    prodacts

---
+++ src/components/prodacts/Prodacts.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./Prodacts.scss";
+import rasim from "../../assets/Group 37.svg";
+import btnRasim from "../../assets/Vector 1.svg";
+
+const bas__url = "https://672fc50866e42ceaf15ea982.mockapi.io";
+const Prodacts = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${bas__url}/products`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log(err))
+      .finally();
+  }, []);
+  console.log(data);
+  const prodactsItm = data?.map((pro) => (
+    <div className="prodacts__main" key={pro.id}>
+      <img className="prodacts__img" src={pro.url} alt="" />
+      <h3 className="prodacts__h3">{pro.title}</h3>
+      <div className="prodacts__div">
+        <div>
+          <del className="prodacts__p">{pro.previousprice}</del>
+
+          <p className="prodacts__p3">{pro.discountprice}₽</p>
+        </div>
+        <img className="prodacts__ktg" src={rasim} alt="" />
+      </div>
+    </div>
+  ));
+  return (
+    <>
+      =
+      {/* <div className="main-div">
+          <h2 className="main-h2">Каталог</h2>
+          <button className="main-btn">
+            Весь каталог
+            <img src={btnRasim} alt="" />
+          </button>
+        </div> */}
+      <div className="conteaner prodacts">{prodactsItm}</div>
+    </>
+  );
+};
+
+export default Prodacts;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Carusel from "../../components/corusel/Carusel";
 import Catolig from "../../components/catolig/Catolig";
+import Prodacts from "../../components/prodacts/Prodacts";
 
 const Home = () => {
   return (
     <div>
       <Carusel />
       <Catolig />
+      <Prodacts />
     </div>
   );
 };
